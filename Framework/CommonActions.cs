78af3510b2a19ae551fa66a91bbff5bfc0de7fa9
Branko Sikirica <branko.sikirica@gmail.com>
2021-11-10 08:59:47 +0100

    added log and common actions

---
+++ Framework/CommonActions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework
+{
+    public static class CommonActions
+    {
+        public static string CurrentTime() => DateTime.UtcNow.ToLocalTime().ToString("dd-MM-yy_HH-mm-ss");
+
+    }
+}
---
+++ Framework/Log.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework
+{
+    public static class Log
+    {
+        public static void Error(string message)
+        {
+            Console.WriteLine($"ERROR [{Time()}]: {message}");
+        }
+
+        public static void Info(string message)
+        {
+            Console.WriteLine($"INFO [{Time()}]: {message}");
+        }
+
+
+        public static void Warning(string message)
+        {
+            Console.WriteLine($"WARNING [{Time()}]: {message}");
+        }
+
+        private static string Time()
+        {
+            return DateTime.UtcNow.AddHours(1).ToString("dd/MM/yyyy HH:mm:ss");
+        }
+    }
+}
