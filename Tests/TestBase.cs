3ee35bcb0c4f367a011ad41c784f2f698bfbcd8c
Branko Sikirica <branko.sikirica@gmail.com>
2021-11-10 09:19:37 +0100

    set anonymous framework

---
+++ Tests/TestBase.cs
@@ -0,0 +1,46 @@
+﻿using Framework;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using System.Diagnostics;
+using TechTalk.SpecFlow;
+
+namespace Tests
+{
+    [Binding]
+    public class TestBase
+    {
+        private Stopwatch _stopwatch = new Stopwatch();
+
+        [BeforeScenario]
+        public void Init()
+        {
+            _stopwatch.Restart();
+            Driver.Initialize();
+        }
+
+        [AfterScenario]
+        public void Cleanup()
+        {
+            _stopwatch.Stop();
+
+
+            if (!TestCompletedWithoutErrors())
+            {
+                TakeScreenshot();
+            }
+
+            Driver.Cleanup();
+        }
+        private void TakeScreenshot()
+        {
+            var screenshot = Driver.TakeScreenshot(TestContext.CurrentContext.Test.Name);
+            if (screenshot != null) TestContext.AddTestAttachment(screenshot);
+        }
+
+        public bool TestCompletedWithoutErrors()
+        {
+            return TestContext.CurrentContext.Result.Outcome.Equals(ResultState.Inconclusive) ||
+                   TestContext.CurrentContext.Result.Outcome.Equals(ResultState.Success);
+        }
+    }
+}
