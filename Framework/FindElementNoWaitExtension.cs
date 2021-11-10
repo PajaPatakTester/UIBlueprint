45fd64b7d9803866861a3539f4a8d2bfbc7b9e33
Branko Sikirica <branko.sikirica@gmail.com>
2021-11-10 09:03:35 +0100

    added find element no wait extension

---
+++ Framework/FindElementNoWaitExtension.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.ObjectModel;
+
+namespace Framework
+{
+    public static class FindElementNoWaitExtension
+    {
+        public static IWebElement FindElementNoWait(this IWebDriver webDriver, By by)
+        {
+            Driver.Instance.Manage().Timeouts().ImplicitWait = TimeSpan.Zero;
+
+            IWebElement result = null;
+            try
+            {
+                result = Driver.Instance.FindElement(by);
+            }
+            catch (Exception)
+            {
+                //  ignored
+            }
+
+            Driver.Instance.Manage().Timeouts().ImplicitWait = Driver.ImplicitWaitTimeout;
+
+            return result;
+        }
+
+        public static ReadOnlyCollection<IWebElement> FindElementsNoWait(this IWebDriver webDriver, By by)
+        {
+            Driver.Instance.Manage().Timeouts().ImplicitWait = TimeSpan.Zero;
+
+            var result = Driver.Instance.FindElements(by);
+
+            Driver.Instance.Manage().Timeouts().ImplicitWait = Driver.ImplicitWaitTimeout;
+
+            return result;
+        }
+
+    }
+}
